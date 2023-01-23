8239f87978ff9c5c871d5b0f8532a5cf3b2ead1b
GaGiiiii <dragoslav.gagi8@gmail.com>
2023-01-23 17:29:11 +0100

    Updated Exception

--- src/Exceptions/InvalidCollectionDataProvidedException.php
+++ src/Exceptions/InvalidCollectionDataProvidedException.php
@@ -1,11 +1,12 @@
 <?php
+
 declare(strict_types=1);
 
 namespace Tymeshift\PhpTest\Exceptions;
 
 class InvalidCollectionDataProvidedException extends \Exception
 {
-    const MESSAGE = 'Invalid data provided for building collection';
+    protected const MESSAGE = 'Invalid data provided for building collection';
 
     public function __construct()
     {
--- src/Exceptions/StorageDataMissingException.php
+++ src/Exceptions/StorageDataMissingException.php
@@ -1,9 +1,15 @@
 <?php
+
 declare(strict_types=1);
 
 namespace Tymeshift\PhpTest\Exceptions;
 
 class StorageDataMissingException extends \Exception
 {
+    protected const MESSAGE = 'Storage data is missing';
 
+    public function __construct()
+    {
+        parent::__construct(self::MESSAGE, 500);
+    }
 }
